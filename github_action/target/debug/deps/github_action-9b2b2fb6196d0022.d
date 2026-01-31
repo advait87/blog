072855126442a3cf7dde7924fbd28e8b1d83f0eb
Advait <advaitandhale87@gmail.com>
2026-01-31 17:05:25 +0530

    Here goes nothing

--- github_action/target/debug/deps/github_action-9b2b2fb6196d0022.d
+++ github_action/target/debug/deps/github_action-9b2b2fb6196d0022.d
@@ -1,6 +1,5 @@
-/Users/rick/blog/github_action/target/debug/deps/github_action-9b2b2fb6196d0022.d: src/main.rs src/template.html
+/Users/rick/blog/github_action/target/debug/deps/github_action-9b2b2fb6196d0022.d: src/main.rs
 
-/Users/rick/blog/github_action/target/debug/deps/libgithub_action-9b2b2fb6196d0022.rmeta: src/main.rs src/template.html
+/Users/rick/blog/github_action/target/debug/deps/libgithub_action-9b2b2fb6196d0022.rmeta: src/main.rs
 
 src/main.rs:
-src/template.html:
